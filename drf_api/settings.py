e8c89a7701bd7a3f0bdcdadb0acfc51b744371f1
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-10-15 10:15:46 +0000

    Add serializer, view and url path, update secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6#)!m_^)c*+%vqv(ykzys=n4gsqk^vwe(n%k+%cs%xve9kkr!n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'profiles',
 ]
 
